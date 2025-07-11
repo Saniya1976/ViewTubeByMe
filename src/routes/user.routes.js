e1e6b08d77048febb638e3c3927e60308d89fe0c
Saniya Kumari <saniyakumari1976@gmail.com>
2025-07-11 23:01:29 +0530

    updated routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,7 @@ import { loginUser } from "../controllers/user.controller.js";
 const router = Router();
 import { upload } from "../middlewares/multer.middleware.js";
 import { logoutUser } from "../controllers/user.controller.js";
+import {refreshAccessToken} from "../controllers/user.controller.js";
 
 router.route("/register").post(
     upload.fields([
@@ -14,4 +15,5 @@ router.route("/register").post(
 );
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT,logoutUser);
+router.route("/refresh-Token").post(refreshAccessToken)
 export default router; 
